d810baf5e333a8e82a89f0a69677cc3097db5f4c
Mark <mark@remarkablemark.org>
2018-07-10 21:32:28 -0400

    build(config): override service worker navigation for not found
    Prevent service worker from returning index page for nonexistent
    route.

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 
 /**
  * Config overrides.
@@ -6,8 +7,35 @@ const webpack = require('webpack');
  * https://github.com/timarney/react-app-rewired#extended-configuration-options
  */
 module.exports = (config, env) => {
-  if (process.env.NODE_ENV === 'production') {
-    config.plugins.push(new webpack.IgnorePlugin(/^react-hot-loader$/));
+  if (process.env.NODE_ENV !== 'production') {
+    return config;
   }
+
+  // prevent service worker from returning index page for nonexistent route
+  // see https://github.com/facebook/create-react-app/issues/3299
+  config.plugins = [
+    ...config.plugins.filter(
+      plugin => plugin.constructor.name !== 'SWPrecacheWebpackPlugin'
+    ),
+    new SWPrecacheWebpackPlugin({
+      dontCacheBustUrlsMatching: /\.\w{8}\./,
+      filename: 'service-worker.js',
+      logger(message) {
+        if (message.indexOf('Total precache size is') === 0) {
+          return;
+        }
+        if (message.indexOf('Skipping static resource') === 0) {
+          return;
+        }
+        console.log(message); // eslint-disable-line no-console
+      },
+      minify: true,
+      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
+    }),
+  ];
+
+  // do not bundle `react-hot-loader`
+  config.plugins.push(new webpack.IgnorePlugin(/^react-hot-loader$/));
+
   return config;
 };
