09146cf1988977092ca4238183ead4dc270e3984
Mark <mark@remarkablemark.org>
2018-07-15 15:00:00 -0400

    feat(helpers): remove `formatAuthor` from util
    This util helper was only used in <Title>.

--- src/helpers/util.js
+++ src/helpers/util.js
@@ -1,39 +1,4 @@
 /**
- * Formats author name(s).
- *
- * @param  {String|Array} author
- * @return {String}
- */
-export const formatAuthor = author => {
-  if (typeof author === 'string') {
-    return author;
-  }
-  if (author instanceof Array) {
-    const authorLength = author.length;
-    switch (authorLength) {
-      case 0:
-        return '';
-      case 1:
-        return author[0];
-      case 2:
-        return author.join(' and ');
-      // greater than 2
-      default:
-        return author.reduce((accumulator, name, index) => {
-          if (index === 0) {
-            return name;
-          }
-          if (index === authorLength - 1) {
-            return accumulator + ', and ' + name;
-          }
-          return accumulator + ', ' + name;
-        }, '');
-    }
-  }
-  return '';
-};
-
-/**
  * Gets key.
  *
  * @param  {String|Object} value
