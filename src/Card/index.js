47301764baf48ffab184d76f687a164d12a00fa6
Mark <mark@remarkablemark.org>
2018-07-15 15:49:24 -0400

    refactor(card): refactor <Card> styles and create props.whiteSpace
    Currently props.whiteSpace=true by default.
    
    The goal is to enable it only when config.renderer="text" since
    newlines should not be preserved when renderer is "html" or
    "markdown".

--- src/Card/index.js
+++ src/Card/index.js
@@ -2,19 +2,42 @@ import React, { Component } from 'react';
 import Paper from '@material-ui/core/Paper';
 import { withStyles } from '@material-ui/core/styles';
 
-const styles = theme => ({
-  paper: {
-    margin: theme.spacing.unit * 2, // 16px
-    padding: theme.spacing.unit * 3, // 24px
-    whiteSpace: 'pre-line', // respect newlines in text
-  },
-});
+/**
+ * Card styles.
+ *
+ * @param  {Object} theme
+ * @return {Object}
+ */
+const styles = theme => {
+  const spacingUnit = theme.spacing.unit;
+
+  return {
+    spacing: {
+      margin: spacingUnit * 2, // 16px
+      padding: spacingUnit * 3, // 24px
+    },
+    'pre-line': {
+      // preserve new lines, collapse spaces and tabs, allow text wrapping
+      // https://developer.mozilla.org/docs/Web/CSS/white-space
+      whiteSpace: 'pre-line',
+    },
+  };
+};
 
 class Card extends Component {
+  static defaultProps = {
+    whiteSpace: 'pre-line',
+  };
+
   render() {
-    const { children, classes, className } = this.props;
+    const { children, classes, className, whiteSpace } = this.props;
+
     return (
-      <Paper className={[classes.paper, className].join(' ')}>{children}</Paper>
+      <Paper
+        className={[classes.spacing, classes[whiteSpace], className].join(' ')}
+      >
+        {children}
+      </Paper>
     );
   }
 }
