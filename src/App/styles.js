63fe9929d41562c33428b48da9f88e7ad81ef673
Mark <mark@remarkablemark.org>
2018-07-14 23:16:38 -0400

    feat(app): set global styles for paragraph and heading elements

--- src/App/styles.js
+++ src/App/styles.js
@@ -2,14 +2,48 @@
  * App styles.
  *
  * @param  {Object} theme
- * @param  {Object} theme.breakpoints
- * @param  {Object} theme.typography
  * @return {Object}
  */
-const styles = ({ breakpoints }) => ({
-  grid: {
-    maxWidth: breakpoints.width('sm'), // 600px
-  },
-});
+const styles = theme => {
+  const { breakpoints, spacing, typography } = theme;
+  const marginBottom = typography.pxToRem(spacing.unit * 1.5); // 12px
+
+  return {
+    '@global': {
+      h1: {
+        ...typography.display3,
+        marginBottom,
+      },
+      h2: {
+        ...typography.display2,
+        marginBottom,
+      },
+      h3: {
+        ...typography.display1,
+        marginBottom,
+      },
+      h4: {
+        ...typography.headline,
+        marginBottom,
+      },
+      h5: {
+        ...typography.title,
+        marginBottom,
+      },
+      h6: {
+        ...typography.subheading,
+        marginBottom,
+      },
+      p: {
+        ...typography.body1,
+        marginBottom,
+      },
+    },
+
+    grid: {
+      maxWidth: breakpoints.width('sm'), // 600px
+    },
+  };
+};
 
 export default styles;
