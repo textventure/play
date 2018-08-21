9a5b4c83022631ca71ed6b1706501bcc3104740a
Mark <mark@remarkablemark.org>
2018-08-20 22:38:24 -0400

    feat(src): render <Theme> as the root in index.js
    This applies the new theme that increases the typography font-size
    is to 18px.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import Theme from './Theme';
 import registerServiceWorker from './registerServiceWorker';
 
-render(<App />, document.getElementById('root'));
+render(
+  <Theme>
+    <App />
+  </Theme>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
