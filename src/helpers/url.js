1969165193c3e568f9b0d4a2c29dbd854198263e
Mark <mark@remarkablemark.org>
2018-09-01 23:06:34 -0400

    feat(helpers): add url helper `searchParams` with tests

---
+++ src/helpers/url.js
@@ -0,0 +1,79 @@
+/**
+ * The beginning character of the query string.
+ *
+ * @constant {String}
+ */
+const QUESTION_MARK = '?';
+
+/**
+ * The substring used to delimit key and value pairs in the query string.
+ *
+ * @constant {String}
+ */
+const SEPARATOR = '&';
+
+/**
+ * The substring used to delimit keys and values in the query string.
+ *
+ * @constant {String}
+ */
+const EQUAL = '=';
+
+/**
+ * Returns URL search param(s).
+ *
+ * @see {@link https://developer.mozilla.org/docs/Web/API/URL/searchParams seachParams}
+ *
+ * @param  {String}        search  - The URL query string.
+ * @param  {String}        [param] - The optional parameter name.
+ * @return {Object|String}         - The search params or matching param value.
+ */
+export const searchParams = (search, param) => {
+  if (typeof search !== 'string') {
+    return;
+  }
+
+  if (search[0] === QUESTION_MARK) {
+    search = search.slice(1);
+  }
+
+  const searchParamsMap = search
+    .split(SEPARATOR)
+    .reduce((accumulator, field) => {
+      let [key, value] = field.split(EQUAL);
+      key = decodeURIComponent(key);
+
+      if (value === undefined) {
+        value = '';
+      } else {
+        value = decodeURIComponent(value);
+      }
+
+      switch (typeof accumulator[key]) {
+        // new key-value
+        case 'undefined':
+          accumulator[key] = value;
+          break;
+
+        // existing key-value, convert to array
+        case 'string':
+          accumulator[key] = [accumulator[key], value];
+          break;
+
+        // existing key-values, push to array
+        case 'object':
+          accumulator[key].push(value);
+          break;
+
+        // no default
+      }
+
+      return accumulator;
+    }, {});
+
+  if (typeof param === 'string') {
+    return searchParamsMap[param];
+  } else {
+    return searchParamsMap;
+  }
+};
---
+++ src/helpers/url.spec.js
@@ -0,0 +1,87 @@
+import { searchParams } from './url';
+
+describe('searchParams', () => {
+  // invalid cases
+  [undefined, null, 42, Object, Array, Function, Date].forEach(arg => {
+    describe(`when arguments=[${arg}]`, () => {
+      it('returns `undefined`', () => {
+        expect(searchParams(arg)).toBe(undefined);
+      });
+    });
+  });
+
+  // test cases
+  [
+    // no starting question mark character
+    {
+      args: ['foo=bar'],
+      expected: { foo: 'bar' },
+    },
+
+    // binary param
+    {
+      args: ['?foo'],
+      expected: { foo: '' },
+    },
+
+    // key with multiple values
+    {
+      args: ['?foo=bar&foo=baz'],
+      expected: { foo: ['bar', 'baz'] },
+    },
+    {
+      args: ['?foo=bar&foo=baz&foo=qux'],
+      expected: { foo: ['bar', 'baz', 'qux'] },
+    },
+    {
+      args: ['?foo=bar&foo=baz&foo=qux&foo&foo'],
+      expected: { foo: ['bar', 'baz', 'qux', '', ''] },
+    },
+
+    // key-value pairs
+    {
+      args: ['?foo=bar'],
+      expected: { foo: 'bar' },
+    },
+    {
+      args: ['?foo=bar&baz=qux'],
+      expected: { foo: 'bar', baz: 'qux' },
+    },
+
+    // mixed
+    {
+      args: ['?foo=bar&baz=qux&baz=quux&corge='],
+      expected: { foo: 'bar', baz: ['qux', 'quux'], corge: '' },
+    },
+
+    // encoded
+    {
+      args: ['?foo%20bar=baz%2Fqux'],
+      expected: { 'foo bar': 'baz/qux' },
+    },
+
+    // with 2 arguments
+    {
+      args: ['?foo=bar', null],
+      expected: { foo: 'bar' },
+    },
+    {
+      args: ['?foo=bar', ''],
+      expected: undefined,
+    },
+    {
+      args: ['?foo=bar', 'bar'],
+      expected: undefined,
+    },
+    {
+      args: ['?foo=bar', 'foo'],
+      expected: 'bar',
+    },
+  ].forEach(({ args, expected }) => {
+    describe(`when arguments=${JSON.stringify(args)}`, () => {
+      it(`returns ${JSON.stringify(expected)}`, () => {
+        expect(searchParams.apply(null, args)).toEqual(expected);
+      });
+    });
+  });
+});
