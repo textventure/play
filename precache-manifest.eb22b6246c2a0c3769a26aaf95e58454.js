b8d03a1125e4d42c5f7686a44e375f5600b871b2
Mark <mark@remarkablemark.org>
2021-01-31 15:44:04 -0500

    gitploy

---
+++ precache-manifest.eb22b6246c2a0c3769a26aaf95e58454.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a00100e9ad2c8d961002acf7f385069",
+    "url": "/play/index.html"
+  },
+  {
+    "revision": "00353fbc75f4a37fd59e",
+    "url": "/play/static/js/2.a33600d2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/play/static/js/2.a33600d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f655c062cda3f60d27e",
+    "url": "/play/static/js/main.deee7375.chunk.js"
+  },
+  {
+    "revision": "fd42f6511b71d104d81d",
+    "url": "/play/static/js/runtime-main.f1b9e660.js"
+  }
+]);
